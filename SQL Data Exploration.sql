d9c3c5f9905d9108e78088362cd066ef42e75307
MRekawek <111175264+MRekawek@users.noreply.github.com>
2022-11-15 14:23:21 +0100

    Update SQL Data Exploration.sql

--- SQL Data Exploration.sql
+++ SQL Data Exploration.sql
@@ -17,7 +17,7 @@ order by 1,2
 
 
 -- Looking at Total Cases vs Total Deaths
--- Shows likelihood of dying if you contract covid in your country
+-- Shows likelihood of dying if you contract covid in my country
 Select Location, date, total_cases, total_deaths,(total_deaths/total_cases)*100 as DeathPercentage
 From PortfolioProject..CovidDeaths
 Where location like '%Poland%'
